374d0b153c7d4f457879b15ae763caf11649b7e7
Jaime van Kessel <nallath@gmail.com>
2023-11-05 13:12:32 +0100

    Add rudimentary flickering for neopixel

--- BaseFirmware/BaseFirmware.ino
+++ BaseFirmware/BaseFirmware.ino
@@ -3,13 +3,69 @@
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
+#include <Adafruit_NeoPixel.h>
 #include "WiFiManager.h"  //https://github.com/tzapu/WiFiManager
 #include <ArduinoOTA.h>
 #include <ArduinoJson.h> 
 
-// https://github.com/FrivolousEngineering/git-describe-arduino
-#include "git-version.h"
 
+#include "git-version.h" // https://github.com/FrivolousEngineering/git-describe-arduino
+
+
+#define NUMPIXELS 12 // Number of pixels in the ledring
+
+#define LEDRINGPIN D2 // Datapin for the ledring
+
+// Any unconnected pin, to try to generate a random seed
+#define UNCONNECTED_PIN         2
+ 
+// The LED can be in only one of these states at any given time
+#define BRIGHT                  0
+#define UP                      1
+#define DOWN                    2
+#define DIM                     3
+#define BRIGHT_HOLD             4
+#define DIM_HOLD                5
+ 
+// Percent chance the LED will suddenly fall to minimum brightness
+#define FLICKER_BOTTOM_PERCENT         10
+// Absolute minimum of the flickering
+#define FLICKER_ABSOLUTE_MIN_INTENSITY 128
+// Minimum intensity during "normal" flickering (not a dramatic change)
+#define FLICKER_MIN_INTENSITY          192
+// Maximum intensity of the flickering
+#define FLICKER_MAX_INTENSITY          255
+ 
+// Decreasing brightness will take place over a number of milliseconds in this range
+#define DOWN_MIN_MSECS          20
+#define DOWN_MAX_MSECS          250
+// Increasing brightness will take place over a number of milliseconds in this range
+#define UP_MIN_MSECS            20
+#define UP_MAX_MSECS            250
+// Percent chance the color will hold unchanged after brightening
+#define BRIGHT_HOLD_PERCENT     20
+// When holding after brightening, hold for a number of milliseconds in this range
+#define BRIGHT_HOLD_MIN_MSECS   0
+#define BRIGHT_HOLD_MAX_MSECS   100
+// Percent chance the color will hold unchanged after dimming
+#define DIM_HOLD_PERCENT        5
+// When holding after dimming, hold for a number of milliseconds in this range
+#define DIM_HOLD_MIN_MSECS      0
+#define DIM_HOLD_MAX_MSECS      50
+ 
+#define MINVAL(A,B)             (((A) < (B)) ? (A) : (B))
+#define MAXVAL(A,B)             (((A) > (B)) ? (A) : (B))
+ 
+// END OF CANDLE MODE RELATED STUFF ////////////////////////////////////////////////////
+
+byte state;
+unsigned long flicker_msecs;
+unsigned long flicker_start;
+byte index_start;
+byte index_end;
+
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, LEDRINGPIN, NEO_GRB + NEO_KHZ800);
 
 char hostString[20] = {0};
 char endpoint[40];
@@ -35,6 +91,49 @@ void saveConfigCallback ()
   shouldSaveConfig = true;
 }
 
+void setFlicker(byte intensity)
+{
+  int led_index; 
+
+  // Clamp intensity between max and absolute min.
+  intensity = MAXVAL(MINVAL(intensity, FLICKER_MAX_INTENSITY), FLICKER_ABSOLUTE_MIN_INTENSITY);
+  
+  if (intensity >= FLICKER_MIN_INTENSITY)
+  {
+    for(led_index = 0; led_index < NUMPIXELS; led_index++)
+    {
+      strip.setPixelColor(led_index, 0,  intensity * 3 / 8, intensity);      
+    }
+  } else {
+    for(led_index = 0; led_index < NUMPIXELS; led_index++)
+    {
+      strip.setPixelColor(led_index, 0, intensity * 3.25 / 8, intensity);
+    }
+  }
+ 
+  strip.show();
+  return;
+}
+
+void setupNeoPixel()
+{
+  // There is no good source of entropy to seed the random number generator,
+  // so we'll just read the analog value of an unconnected pin.  This won't be
+  // very random either, but there's really nothing else we can do.
+  //
+  // True randomness isn't strictly necessary, we just don't want a whole
+  // string of these things to do exactly the same thing at the same time if
+  // they're all powered on simultaneously.
+  randomSeed(analogRead(UNCONNECTED_PIN));
+ 
+  setFlicker(255);
+  index_start = 255;
+  index_end = 255;
+  state = BRIGHT;
+
+  strip.begin();
+}
+
 void setup() 
 {
   // put your setup code here, to run once:
@@ -50,6 +149,8 @@ void setup()
 
   Serial.println("Firmware version:");
   Serial.println(GIT_VERSION);
+
+  setupNeoPixel();
   
   // Clear the data (used for debugging)
   // SPIFFS.format(); // This shouldn't be done in live production code since it will reset all data that it has. 
@@ -233,9 +334,87 @@ void loop() {
     ESP.restart();
   }
 
-  delay(2000);
+
+  neopixelLoop();
 }
 
+void neopixelLoop()
+{
+  unsigned long current_time; 
+  current_time = millis();
+  Serial.println("loop!");
+  switch (state)
+  {
+    case BRIGHT:
+    {   
+      Serial.println("Bright"); 
+      flicker_msecs = random(DOWN_MAX_MSECS - DOWN_MIN_MSECS) + DOWN_MIN_MSECS;
+      flicker_start = current_time;
+      index_start = index_end;
+      if ((index_start > FLICKER_ABSOLUTE_MIN_INTENSITY) && (random(100) < FLICKER_BOTTOM_PERCENT))
+      {
+        index_end = random(index_start - FLICKER_ABSOLUTE_MIN_INTENSITY) + FLICKER_ABSOLUTE_MIN_INTENSITY;
+      } else {
+        index_end = random(index_start - FLICKER_MIN_INTENSITY) + FLICKER_MIN_INTENSITY;
+      }
+ 
+      state = DOWN;
+      break;  
+    }  
+    case DIM:
+    {
+      Serial.println("Dim");
+      flicker_msecs = random(UP_MAX_MSECS - UP_MIN_MSECS) + UP_MIN_MSECS;
+      flicker_start = current_time;
+      index_start = index_end;
+      index_end = random(FLICKER_MAX_INTENSITY - index_start) + FLICKER_MIN_INTENSITY;
+      state = UP;
+      break;
+    }
+    case BRIGHT_HOLD:  
+    case DIM_HOLD:
+    {
+      Serial.println("DIM Hold");
+      if (current_time >= (flicker_start + flicker_msecs))
+      {
+        state = (state == BRIGHT_HOLD) ? BRIGHT : DIM; 
+      }
+      break;
+    }
+    case UP:
+    case DOWN:
+    {
+      //  Serial.println("Down");
+      if (current_time < (flicker_start + flicker_msecs)) {
+        setFlicker(index_start + ((index_end - index_start) * (((current_time - flicker_start) * 1.0) / flicker_msecs)));
+      } else {
+        setFlicker(index_end);
+ 
+        if (state == DOWN)
+        {
+          if (random(100) < DIM_HOLD_PERCENT)
+          {
+            flicker_start = current_time;
+            flicker_msecs = random(DIM_HOLD_MAX_MSECS - DIM_HOLD_MIN_MSECS) + DIM_HOLD_MIN_MSECS;
+            state = DIM_HOLD;
+          } else {
+            state = DIM;
+          } 
+        } else {
+          if (random(100) < BRIGHT_HOLD_PERCENT)
+          {
+            flicker_start = current_time;
+            flicker_msecs = random(BRIGHT_HOLD_MAX_MSECS - BRIGHT_HOLD_MIN_MSECS) + BRIGHT_HOLD_MIN_MSECS;
+            state = BRIGHT_HOLD;
+          } else {
+            state = BRIGHT;
+          }
+        }
+      }
+      break;
+    }
+  }
+}
 
 void hostProbeResult(String p_pcDomainName, bool p_bProbeResult) 
 {
